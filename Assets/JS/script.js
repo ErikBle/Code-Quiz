41c4911679afd461fc881404026a4c9efb3ca052
Yfopla <ekblecker@gmail.com>
2020-02-19 10:05:46 -0500

    Setup variables i will need and started functions

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,4 +1,4 @@
-var triviaquestion = [
+var triviaQuestion = [
     {
         question: "What is the short form for League of Legends?",
         answerList: ["LoL", "lol", "LeagueOL"],
@@ -7,11 +7,73 @@ var triviaquestion = [
     {
         question: "How many roles are there in League?",
         answerList: ["2", "4", "5"],
-        answer: 1
+        answer: 2
     },
     {
         question: "What is the shortest time you can surrender?",
-        choices: ["5 min", "15 min", "20 min"],
-        correctAnswer: 1
-    }
+        answerList: ["5 min", "15 min", "20 min"],
+        answer: 1
+    },
+    {
+        question: "How many different kind of drakes are there?",
+        answerList: ["2", "3", "4"],
+        answer: 2
+    },
+    {
+        question: "How many Inhibators, IN TOTAL, are there?",
+        answerList: ["6", "9", "14"],
+        answer: 0
+    },
+    {
+        question: "The game ends when the enemy __ is destoryed",
+        answerList: ["Boss", "Nexus", "Dota"],
+        answer: 1
+    },
 ];
+
+var outputs = {
+
+    correct: "Correct :) ",
+    incorrect: "Incorrect :( ",
+    timing: "Out of Time!",
+    over: "Quiz is up!"
+}
+
+var currentQuestion;
+var correctAnswers;
+var incorrectAnswers;
+var unanswered;
+var answered;
+var userChoice;
+var seconds;
+var time;
+
+$('#startBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+$('#startOverBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+function newGame()
+{
+
+    $("#final-msg").empty();
+    $("#correctanswers").empty();
+    $("#incorrectanswers").empty();
+    $("#unanswered").empty();
+
+    currentQuestion = 0;
+    correctAnswers = 0;
+    incorrectAnswers = 0;
+    unanswered = 0;
+    newQus();
+}
+
+
+
+
+
