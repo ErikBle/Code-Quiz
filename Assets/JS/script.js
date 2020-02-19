cbb1452324800c71dd9d95e7d3c651e2bff7574a
Yfopla <ekblecker@gmail.com>
2020-02-19 09:45:47 -0500

    restarted project. Had some tips from a friend

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,27 +1,17 @@
-var btn1 = document.createElement("button");
-var btn2 = document.createElement("button");
-var btn3 = document.createElement("button");
-var btn4 = document.createElement("button");
-var startBtn = document.querySelector("#startBtn");
-
-function startQuiz() {
-    document.querySelector(".btn-group-vertical").appendChild(btn1);
-    document.querySelector(".btn-group-vertical").appendChild(btn2);
-    document.querySelector(".btn-group-vertical").appendChild(btn3);
-    document.querySelector(".btn-group-vertical").appendChild(btn4);
-    btn1.className = "btn btn-primary";
-    btn2.className = "btn btn-primary";
-    btn3.className = "btn btn-primary";
-    btn4.className = "btn btn-primary";
-    document.getElementById("startBtn").style.display = "none";
-    document.querySelector("p").style.display = "none";
-};
-
-
-startBtn.addEventListener("click", startQuiz);
-
-
-//Links to the Highscore page when clicked
-document.getElementById("HSbtn").onclick = function () {
-    location.href = "./Assets/Pages/highscore.html";
-};
+var triviaquestion = [
+    {
+        question: "What is the short form for League of Legends?",
+        answerList: ["LoL", "lol", "LeagueOL"],
+        answer: 0
+    },
+    {
+        question: "How many roles are there in League?",
+        answerList: ["2", "4", "5"],
+        answer: 1
+    },
+    {
+        question: "What is the shortest time you can surrender?",
+        choices: ["5 min", "15 min", "20 min"],
+        correctAnswer: 1
+    }
+];
